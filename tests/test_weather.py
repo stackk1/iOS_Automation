1e453d94d934a89920c8501b3991501c0449ae02
stackk1 <74647121+stackk1@users.noreply.github.com>
2024-06-23 09:55:14 -0700

    add game tests

--- tests/test_weather.py
+++ tests/test_weather.py
@@ -1,12 +1,12 @@
 #Tests for Weather App - Andrew Stack 2024
 from screens.weather_screen import WeatherScreen
-from screens.launch_screen import LaunchScreen
+from screens.launch_screen import LaunchScreen, App
 
 
 class TestWeather:
 
     def test_open_weather(self):
         launch_screen = LaunchScreen()
-        launch_screen.launch_weather()
+        launch_screen.launch_app(App.WEATHER)
         weather_screen = WeatherScreen()
         assert weather_screen.is_here()
