809e49d6e7bc9d58dbd9e4d7a3abe17acfb12e8c
stackk1 <astackk@icloud.com>
2024-07-15 15:02:29 -0700

    add tests for book favouriting

--- screens/books_screen.py
+++ screens/books_screen.py
@@ -10,8 +10,8 @@ class Book(Enum):
     Amazing_Words = "Amazing_Words"
     Literature_For_Reading = "Literature_For_Reading"
     TEXT_AND_MORE = "Text_and_More"
-    Characters_Words_and_Paragraphs = "Characters,_Words,_and_Paragraphs"
-    A_Book_I_Think = "A_Book,_I_Think"
+    Characters_Words_and_Paragraphs = "Characters_Words_and_Paragraphs"
+    A_Book_I_Think = "A_Book_I_Think"
 
 
 class BooksScreen(BaseScreen):
@@ -41,24 +41,28 @@ class BooksScreen(BaseScreen):
 
     def add_book_as_favourite(self):
         try:
-            self.is_book_favourite()
-            print("book already in favourites")
-        except NoSuchElementException:
-            print("failed to set title as favourite")
+            if self.is_book_favourite() is True:
+                print("book already in favourites")
+            else:
+                self.driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="BUTTON_SET_BOOK_FAVOURITE").click()
         except:
-            self.driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="BUTTON_SET_BOOK_FAVOURITE").click()
+            print("failed to set title as favourite")
 
     def remove_books_from_favourites(self):
         try:
-            self.is_book_favourite()
-            self.driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="BUTTON_REMOVE_BOOK_FAVOURITE").click()
+            if self.is_book_favourite() is True:
+                self.driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="BUTTON_REMOVE_BOOK_FAVOURITE").click()
+            else:
+                print("book is not a favourite")
         except NoSuchElementException:
             print("failed to remove book from favourites")
 
     def is_book_favourite(self) -> bool:
         try:
-            self.driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="BUTTON_REMOVE_BOOK_FAVOURITE")
-            return True
+            if self.driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="BUTTON_REMOVE_BOOK_FAVOURITE"):
+                return True
+            else:
+                return False
         except NoSuchElementException:
-            print("title is not a favourite")
+            print("unable to determine favourite status")
             return False
